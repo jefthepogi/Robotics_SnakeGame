d1735e83ebc84600d4b4eaab6178a4e190723423
UnknownUser999 <97450221+jefthepogi@users.noreply.github.com>
2023-12-08 22:00:20 +0800

    Refactored almost all the code.
    Changed the whole system of the game, by implementing a grid based movement.

--- snake.ino
+++ snake.ino
@@ -2,11 +2,17 @@
 
 LiquidCrystal_I2C lcd(32, 16, 2);
 
-struct vec{
+struct vec {
   int x, y;
+  vec(int x = 0, int y = 0) : x(x), y(y) {}
 };
 
-// based on code by Majenko @ https://arduino.stackexchange.com/a/48244
+struct node {
+  node *prev;
+  node *next;
+  vec pos;
+};
+
 class Button {
     private:
         bool _state;
@@ -30,177 +36,143 @@ class Button {
         }
 };
 
+Button A(13);
+Button B(12);
 
-Button UP(6);
-Button DOWN(4);
-Button LEFT(7);
-Button RIGHT(5);
+short time_since_last_draw = 0;
+const short MIN_DRAW_WAIT = 60;
+unsigned long last_update = 0;
 
-vec axis = {0, 0};
-vec grid = {5, 8};
-vec dir = {0, 0};
-vec DIR[] = {{0, 1},// UP
-			{0,-1},// DOWN
-			{-1, 0},//LEFT
-			{1, 0}};//RIGHT
+vec mat = vec(5, 8);
+vec sectors = vec(16, 2);
+vec mapSize = vec(mat.x * sectors.x, mat.y * sectors.y);
 
-int row = 4;
-byte lastPos = B00100;
+struct node *s_head = NULL;
 
-byte snake[] = {
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000
+enum Direction {
+  LEFT = 1,
+  DOWN = LEFT << 1,
+  RIGHT = LEFT << 2,
+  UP = LEFT << 3,
 };
 
-void updateSnake();
-void Input();
+Direction currDir = UP;
+
+void initChar(byte* character)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    character[i] = B00000;
+  }
+}
+
+void initGameMap()
+{
+  byte SECTORS[sectors.x][sectors.y][mat.y];
+  
+  for (int i = 0; i < sectors.y; i++)
+  {
+    for (int j = 0; j < sectors.x; j++)
+    {
+      initChar(SECTORS[j][i]);
+    }
+  }
+ 
+  for (node* temp = s_head; temp != NULL; temp = temp->next)
+  {
+    vec p = vec(temp->pos.x, temp->pos.y);
+    vec curr_sec = vec(p.x/mat.x, p.y/mat.y);
+    SECTORS[curr_sec.x][curr_sec.y][p.y - (curr_sec.y * mat.y)] = B10000 >> (p.x - (curr_sec.x * mat.x));
+    //byte x = B11111 << 10;
+    //for (int i = 0; i < 5; i++) Serial.print(bitRead(x, i));
+    //Serial.print(curr_sec.x); Serial.print(" "); Serial.println(curr_sec.y);
+  }
+  
+  byte currChar = 0;
+  
+  for (int i = 0; i < sectors.y; i++)
+  {
+    for (int j = 0; j < sectors.x; j++)
+    {
+      for (int h = 0; h < mat.y; h++)
+      {
+        if (SECTORS[j][i][h])
+        {
+          lcd.createChar(currChar, SECTORS[j][i]);
+          lcd.setCursor(j, i);
+          lcd.write(byte(currChar));
+          currChar++;
+          //Serial.println("fck");
+          break;
+        }
+
+      }
+    }
+  }
+      
+}
+
+void moveSnake()
+{
+  switch (currDir)
+  {
+    case LEFT:
+      s_head->pos.x--;
+    break;
+    case DOWN:
+      s_head->pos.y++;
+    break;
+    case RIGHT:
+      s_head->pos.x++;
+    break;
+    case UP:
+      s_head->pos.y--;
+    break;
+  }
+}
+
+void control()
+{
+  if (A.isReleased())
+  {
+    if (currDir == LEFT)
+    {
+      currDir = UP;
+    }
+    else {
+      currDir = static_cast<Direction>(currDir << 1);
+      //Serial.println("fkoff");
+    }
+  }
+  else if (B.isReleased())
+  {
+    if (currDir == UP)
+    {
+      currDir = LEFT;
+    }
+    else {
+      currDir = static_cast<Direction>(currDir << 1);
+    }
+  }
+}
 
 void setup()
-{
-  Serial.begin(9600);
+{  
   lcd.init();
   lcd.backlight();
+  Serial.begin(9600);
+  //Serial.print();
+  //Serial.println();
+  s_head = new node();
+  s_head->prev = NULL;
+  s_head->prev = NULL;
+  s_head->pos = vec(mapSize.x - 24, mapSize.y - 20);
+
 }
 
 void loop()
 {
-  Input();
-  updateSnake();
-}
-
-void Input()
-{
-  
-  if (UP.isReleased() && dir.y == 0)
-  {
-    dir = {0, 1};
-    snake[row] = lastPos;
-  }
-  else if (DOWN.isReleased() && dir.y == 0)
-  {
-    dir = {0, -1};
-    snake[row] = lastPos;
-  }
-  else if (LEFT.isReleased() && dir.x == 0)
-  {
-    dir = {-1, 0};
-    snake[row] = lastPos;
-  }
-  else if (RIGHT.isReleased() && dir.x == 0)
-  {
-    dir = {1, 0};
-    snake[row] = lastPos;
-  }
-}
-
-void updateSnake()
-{
-  // Serial.println(row);
-
-  if (dir.y == DIR[1].y ) // DOWN
-  {
-     if (row > grid.y - 1)
-    {
-      axis.y++;
-      if (axis.y > 1)
-      {
-        axis.y = 0;
-      }
-      snake[row-1] = B00000;
-      row = 0;
-      lcd.clear();
-      delay(100);
-      return;
-    }
-
-    snake[row] = lastPos;
-
-    lcd.createChar(0, snake);
-    lcd.setCursor(axis.x, axis.y);
-    lcd.write((byte)0);
-    
-	snake[row] = B00000;
-    row++;
-  }
-  
-  if (dir.y == DIR[0].y) // UP
-  {
-    if (row < 0)
-    {
-      axis.y--;
-      if (axis.y < 0)
-      {
-        axis.y = 1;
-      }
-      
-      snake[row+1] = B00000;
-      row = grid.y - 1;
-	  lcd.write(" ");
-      return;
-    }
-    
-    snake[row] = lastPos;
-
-    lcd.createChar(0, snake);
-    lcd.setCursor(axis.x, axis.y);
-    lcd.write((byte)0);
-    
-    snake[row] = B00000;
-    row--;
-  }
-  
-  if (dir.x == DIR[2].x) // LEFT
-  {
-    snake[row] = snake[row] << 1;
-    lastPos = snake[row];
-
-    lcd.createChar(0, snake);
-    lcd.setCursor(axis.x, axis.y);
-    lcd.write((byte)0);
-    
-    if (snake[row] == B00000)
-    {
-      axis.x--;
-      if (axis.x < 0)
-      {
-        axis.x = 15;
-      }
-      
-      snake[row] = B00001;
-      lastPos = snake[row];
-      lcd.write(" ");
-      return;
-    }
-  }
-  
-  if (dir.x == DIR[3].x) // RIGHT
-  {
-    snake[row] = snake[row] >> 1;
-    lastPos = snake[row];
-
-    lcd.createChar(0, snake);
-    lcd.setCursor(axis.x, axis.y);
-    lcd.write((byte)0);
-    
-    if (snake[row] == B00000)
-    {
-      axis.x++;
-      if (axis.x > 15)
-      {
-        axis.x = 0;
-      }
-      
-      snake[row] = B10000;
-      lastPos = snake[row];
-      lcd.write(" ");
-      return;
-    }
-  }
-  
+  control();
+  moveSnake();
+  initGameMap();
 }
